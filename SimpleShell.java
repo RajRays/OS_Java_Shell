54f8933da453f43ecc087f597652ead1f40dd63f
RajRays <Raj@Fameria.com>
2017-10-07 00:07:28 -0400

    Steps 1-4 Completed Must implement Part2 and Part3 along with Step 5

--- SimpleShell.java
+++ SimpleShell.java
@@ -1,8 +1,7 @@
 package com.rajendra.bhagroo;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class SimpleShell {
 
@@ -11,23 +10,63 @@ public class SimpleShell {
         String commandLine;
         BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
 
-        //Break out with <control><C>
+        ArrayList<String> userCommands = new ArrayList<String>();
+        ProcessBuilder pb = new ProcessBuilder();
+
+
+        //Start Shell, Break Out With <Control><C>
         while (true) {
-            //Read user input
-            System.out.println("jsh>");
+            //Read User Input
+            System.out.print("jsh>");
             commandLine = console.readLine();
 
-            //If user entered a return, loop again
+            //If User Entered A Return, Loop Again
             if(commandLine.equals(""))
                 continue;
 
+            //Separate Commands Within commandLine variable
+            StringTokenizer commands = new StringTokenizer(commandLine);
+
+            //Adding Commands To ArrayList
+            while(commands.hasMoreTokens()) {
+                userCommands.add(commands.nextToken());
+            }
+
+
+
+
+            /* CODE FOR PART 2 AND 3 HERE*/
+
+
+
+
+            //Start Process from ProcessBuilder
+            pb.command(userCommands);
+            Process process = pb.start();
+
+            //Establish Link To Read Process Output
+            InputStream iStream = process.getInputStream();
+            InputStreamReader iStreamReader = new InputStreamReader(iStream);
+            BufferedReader br = new BufferedReader(iStreamReader);
+
+            //Reading Output Of Process
+            String line;
+
+            while ( (line = br.readLine()) != null )
+                System.out.println(line);
+
+            br.close();
+
+
+
             /** The Steps Are...
-             * (1) Parse the input to obtain the command and any parameters
-             * (2) Create a ProcessBuilder object
-             * (3) Start the process
-             * (4) Obtain the output stream
-             * (5) Output the contents returned by the command
+             * (1) Parse the input to obtain the command and any parameters <DONE>
+             * (2) Create a ProcessBuilder object <DONE>
+             * (3) Start the process <DONE>
+             * (4) Obtain the output stream <DONE>
+             * (5) Output the contents returned by the command <WORKING ON IT>
              */
-        }
-    }
-}
+
+        }//END SHELL
+    }//END MAIN
+}//END CLASS
